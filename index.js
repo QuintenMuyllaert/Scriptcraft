bb096ee459ab16cb5afed592e5bb235644dc41c4
ComputerQ <quinten.muyllaert@outlook.com>
2022-02-17 04:48:55 +0100

    running minecraft server in node

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Starting Scriptcraft!");
+
+import { spawn } from "child_process";
+
+const server = spawn("java", ["-jar", "-Xms2G", "-Xmx2G", "server.jar"], { cwd: "./minecraft" });
+
+server.stdout.on("data", (buffer) => {
+	console.log(buffer.toString());
+});
