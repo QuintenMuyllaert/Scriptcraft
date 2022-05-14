99c29617b26b1de98665cbca3b0b92ac2edeff9f
ComputerQ <quinten.muyllaert@outlook.com>
2022-05-14 15:41:42 +0200

    🚸 automatic minecraft server jar downloader

---
+++ downloadmc.js
@@ -0,0 +1,34 @@
+import { exec } from "child_process";
+import fs from "fs";
+
+let mayServerStart = false;
+
+export const canServerStart = () => {
+	return mayServerStart;
+};
+
+export const downloadLatest = () => {
+	const text2 = fs.readFileSync(".latestmc").toString();
+	if (fs.existsSync(".currentmc")) {
+		const text = fs.readFileSync(".currentmc").toString();
+		if (text == text2) {
+			console.log("Already on last MC version, skipping download.");
+			return;
+		}
+	}
+
+	console.log("Downloading latest MC server...");
+	const downloader = exec(`wget -O ./minecraft/server.jar ${text2}`, (error, stdout, stderr) => {
+		const out = stderr.toString();
+		if (out.toString().includes("'./minecraft/server.jar' saved")) {
+			console.log("Latest MC dowloaded!");
+			fs.writeFileSync(".currentmc", text2);
+			mayServerStart = true;
+		}
+	});
+};
+
+export default {
+	downloadLatest,
+	canServerStart,
+};
--- index.js
+++ index.js
@@ -6,39 +6,54 @@ import fs from "fs";
 import { spawn, fork } from "child_process";
 import file from "./modules/file.js";
 
-//remove old Minecraft world
-fs.rmSync(path.join("./minecraft", "world"), { recursive: true, force: true });
+import { downloadLatest, canServerStart } from "./downloadmc.js";
 
-//copies clean Minecraft world
-file.cp(path.join("./minecraft", "world_clean"), path.join("./minecraft", "world"));
+const delay = (ms) => new Promise((r) => setTimeout(r, ms));
 
-const server = spawn("java", ["-jar", "-Xms2G", "-Xmx2G", "server.jar"], { cwd: "./minecraft" });
+(async () => {
+	await downloadLatest();
 
-//hot reload
-let scriptcraft;
-const spawnScriptcraft = () => {
-	scriptcraft = fork("./modules/scriptcraft.js");
-	scriptcraft.alive = true;
+	//remove old Minecraft world
+	fs.rmSync(path.join("./minecraft", "world"), { recursive: true, force: true });
 
-	scriptcraft.on("message", (msg) => {
-		server.stdin.write(msg.toString() + "\n");
-	});
+	//copies clean Minecraft world
+	file.cp(path.join("./minecraft", "world_clean"), path.join("./minecraft", "world"));
 
-	scriptcraft.on("close", () => {
-		scriptcraft.alive = false;
-		scriptcraft.kill();
-		spawnScriptcraft();
-	});
-};
-
-spawnScriptcraft();
-
-fs.watch("./modules", () => {
-	scriptcraft.kill();
-});
-
-server.stdout.on("data", (buffer) => {
-	if (scriptcraft.alive) {
-		scriptcraft.send(buffer.toString().replace("\n", ""));
+	let time = 0;
+	while (!canServerStart()) {
+		console.log(time + " Waiting for server to download...");
+		await delay(1000);
+		time++;
 	}
-});
+
+	const server = spawn("java", ["-jar", "-Xms2G", "-Xmx2G", "server.jar"], { cwd: "./minecraft" });
+
+	//hot reload
+	let scriptcraft;
+	const spawnScriptcraft = () => {
+		scriptcraft = fork("./modules/scriptcraft.js");
+		scriptcraft.alive = true;
+
+		scriptcraft.on("message", (msg) => {
+			server.stdin.write(msg.toString() + "\n");
+		});
+
+		scriptcraft.on("close", () => {
+			scriptcraft.alive = false;
+			scriptcraft.kill();
+			spawnScriptcraft();
+		});
+	};
+
+	spawnScriptcraft();
+
+	fs.watch("./modules", () => {
+		scriptcraft.kill();
+	});
+
+	server.stdout.on("data", (buffer) => {
+		if (scriptcraft.alive) {
+			scriptcraft.send(buffer.toString().replace("\n", ""));
+		}
+	});
+})();
